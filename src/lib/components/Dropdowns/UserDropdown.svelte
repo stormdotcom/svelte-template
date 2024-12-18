96fe42463ede3d63125c853f7073f236642f3c03
Ajmal Nasumudeen <ajmaln73@gmail.com>
2024-12-18 15:10:50 +0530

    added

--- src/lib/components/Dropdowns/UserDropdown.svelte
+++ src/lib/components/Dropdowns/UserDropdown.svelte
@@ -1,80 +1,78 @@
 <script>
-// @ts-nocheck
-
-  import { createPopper } from "@popperjs/core";
-  import '../../../app.css'; // adjust as needed based on your folder depth
-
-
-  const image = "/assets/img/ajmal.jpg";
-
-  let dropdownPopoverShow = false;
-  let btnDropdownRef;
-  let popoverDropdownRef;
-
-  const toggleDropdown = (event) => {
-    event.preventDefault();
-    if (dropdownPopoverShow) {
-      dropdownPopoverShow = false;
-    } else {
-      dropdownPopoverShow = true;
-      createPopper(btnDropdownRef, popoverDropdownRef, {
-        placement: "bottom-start",
-      });
-    }
-  };
-</script>
-
-<div>
-  <a
-    class="text-blueGray-500 block"
-    href="#"
-    bind:this={btnDropdownRef}
-    on:click={toggleDropdown}
-  >
-    <div class="items-center flex">
-      <span
-        class="w-12 h-12 text-sm text-white bg-blueGray-200 inline-flex items-center justify-center rounded-full"
+  // @ts-nocheck
+    import { createPopper } from "@popperjs/core";
+    import '../../../app.css'; // adjust as needed based on your folder depth
+  
+    const image = "/assets/img/ajmal.jpg";
+  
+    let dropdownPopoverShow = false;
+    let btnDropdownRef;
+    let popoverDropdownRef;
+  
+    const toggleDropdown = (event) => {
+      event.preventDefault();
+      dropdownPopoverShow = !dropdownPopoverShow;
+  
+      if (dropdownPopoverShow) {
+        createPopper(btnDropdownRef, popoverDropdownRef, {
+          placement: "bottom-start",
+        });
+      }
+    };
+  </script>
+  
+  <div>
+    <button
+      class="text-blueGray-500 block"
+      aria-label="Toggle dropdown"
+      bind:this={btnDropdownRef}
+      on:click={toggleDropdown}
+    >
+      <div class="items-center flex">
+        <span
+          class="w-12 h-12 text-sm text-white bg-blueGray-200 inline-flex items-center justify-center rounded-full"
+        >
+          <img
+            alt="Profile"
+            class="w-full rounded-full align-middle border-none shadow-lg"
+            src={image}
+          />
+        </span>
+      </div>
+    </button>
+    <div
+      bind:this={popoverDropdownRef}
+      class={`bg-white text-base z-50 float-left py-2 list-none text-left rounded shadow-lg min-w-48 ${dropdownPopoverShow ? 'block' : 'hidden'}`}
+    >
+      <button
+        class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
+        aria-label="Perform Action"
+        on:click={(e) => e.preventDefault()}
       >
-        <img
-          alt="Profile"
-          class="w-full rounded-full align-middle border-none shadow-lg"
-          src={image}
-        />
-      </span>
+        Action
+      </button>
+      <button
+        class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
+        aria-label="Perform Another Action"
+        on:click={(e) => e.preventDefault()}
+      >
+        Another action
+      </button>
+      <button
+        class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
+        aria-label="Perform Something Else"
+        on:click={(e) => e.preventDefault()}
+      >
+        Something else here
+      </button>
+      <div class="h-0 my-2 border border-solid border-blueGray-100"></div>
+      <button
+        class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
+        aria-label="Perform Separated Link Action"
+        on:click={(e) => e.preventDefault()}
+      >
+        Separated link
+      </button>
     </div>
-  </a>
-  <div
-    bind:this={popoverDropdownRef}
-    class={`bg-white text-base z-50 float-left py-2 list-none text-left rounded shadow-lg min-w-48 ${dropdownPopoverShow ? 'block' : 'hidden'}`}
-  >
-    <a
-      href="#"
-      on:click={(e) => e.preventDefault()}
-      class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
-    >
-      Action
-    </a>
-    <a
-      href="#"
-      on:click={(e) => e.preventDefault()}
-      class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
-    >
-      Another action
-    </a>
-    <a
-      href="#"
-      on:click={(e) => e.preventDefault()}
-      class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
-    >
-      Something else here
-    </a>
-    <div class="h-0 my-2 border border-solid border-blueGray-100"></div>
-    <a
-      href="#"
-      on:click={(e) => e.preventDefault()}
-      class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
-    >
-      Separated link
-    </a>
   </div>
-</div>
+  
